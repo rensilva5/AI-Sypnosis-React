2044ff2a2c5bfb883aebe684a371c9526fe952c6
rensilva5 <renzoasilva@gmail.com>
2023-10-19 01:19:01 -0400

    Adding href to links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,23 @@ const Footer = () => {
     <footer className="w-full bg-blue-800 text-white py-4 rounded-md">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <p>&copy; 2023 AI-Sypnosis</p>
+          <p>&copy; 2023 AI-Sypnosis. All rights reserved</p>
         </div>
         <div>
-          <a href="#" className="text-white mr-4">
-            Home
+          <a
+            href="https://github.com/rensilva5/AI-Sypnosis-React"
+            className="text-white mr-4"
+          >
+            Github
           </a>
-          <a href="#" className="text-white mr-4">
-            About
+          <a
+            href="https://www.linkedin.com/in/renzo-silva/"
+            className="text-white mr-4"
+          >
+            Linkedin
           </a>
-          <a href="#" className="text-white">
-            Contact
+          <a href="https://renzosilva.net/" className="text-white">
+            renzosilva.net
           </a>
         </div>
       </div>
